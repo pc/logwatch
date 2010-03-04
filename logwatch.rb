d8c0204b6905160c4a0759c0c262ccc044051eee
root <root@Ubuntu-910-karmic-64-minimal.(none)>
2010-03-05 00:56:35 +0100

    watch cmds

--- logwatch.rb
+++ logwatch.rb
@@ -1,16 +1,38 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'tmail'
 require 'net/smtp'
+require 'tempfile'
 
 class LogWatch
-  def size; File.stat(@path).size; end
+  def size
+    update_cmd if @cmd
+    File.stat(path).size
+  end
+
+  def path; @cmd ? cmd_path : @path; end
+  def thing; @cmd ? @cmd : @path; end
+
+  def update_cmd
+    File.open(cmd_path, 'w') {|f| f.write(`#{@cmd}`)}
+  end
+
+  def cmd_path; @cmd_path ||= Tempfile.new(__FILE__).path; end
+
   def update_size; @size = size; end
   def last_size; @size; end
   
-  def initialize(path, to, from="logwatch@#{`hostname`.strip}")
-    @path = path
+  def initialize(path_or_cmd, to, from="logwatch@#{`hostname`.strip}")
     @to = to
     @from = from
+
+    if path_or_cmd =~ /^!/
+      @cmd = path_or_cmd[1..-1]
+      update_cmd
+    else
+      @path = path_or_cmd
+    end
 
     if exists?
       @size = size
@@ -20,7 +42,7 @@ class LogWatch
   end
   
   def diff(sz)
-    File.open(@path, 'r') do |f|
+    File.open(path, 'r') do |f|
       f.seek(last_size)
       f.read(sz - last_size)
     end
@@ -31,16 +53,16 @@ class LogWatch
     m.to = @to
     m.from = @from
     m.body = diff
-    m.subject = "Change in #{@path}"
+    m.subject = "Change in #{path}"
     m.date = Time.now
 
     Net::SMTP.start('localhost', 25) {|cli| cli.send_message(m.to_s, @from, @to)}
   end
 
-  def exists?; File.exists?(@path); end
+  def exists?; File.exists?(path); end
 
   def announce
-    $stderr.puts "Watching #{@path}"
+    $stderr.puts "Watching #{thing}"
   end
   
   def watch
